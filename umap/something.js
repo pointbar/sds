7316c0e6b1424b7a39f209616bd4c56f1f0a5812
pntbr <stephane.langlois@scopyleft.fr>
2018-04-18 17:29:00 +0200

    Change marker color depending on price

--- umap/something.js
+++ umap/something.js
@@ -2,12 +2,11 @@ const fs = require('fs')
 const GeoJSON = require('geojson')
 
 const cities = [
-  { name: 'Paris', description: 'Prix de l\'eau', lat: 48.8534, lng: 2.3488 },
-  { name: 'Toulouse', description: 'Prix de l\'eau', lat: 43.6, lng: 1.433333 },
+  { name: 'Paris', description: '.79', lat: 48.8534, lng: 2.3488 },
+  { name: 'Toulouse', description: '6.30', lat: 43.6, lng: 1.433333 },
 ]
 
 const data = addMarkerToCities(cities)
-
 const dataGeoJson = GeoJSON.parse(data , {Point: ['lat', 'lng']})
 
 fs.writeFileSync('umap.geojson', JSON.stringify(dataGeoJson, null, 4)) 
@@ -16,16 +15,19 @@ fs.writeFileSync('umap.geojson', JSON.stringify(dataGeoJson, null, 4))
 // FUNCTIONS
 
 function addMarkerToCities (cities) {
-	const umapMarker = {
-		"color": "LimeGreen",
-		"iconClass": "Drop",
-		"zoomTo": 20,
-		"showLabel": true,
-		"labelHover": true
-	}
-	
 	return cities.map(city => {
-		city._storage_options = umapMarker
+		const price = +city.description
+
+		city._storage_options = {
+			iconClass: 'Drop'
+		}
+
+		if (price < 1) {
+			city._storage_options.color = 'LimeGreen'
+		} else {
+			city._storage_options.color = 'DarkRed'
+		}
+
 		return city
 	})
 }

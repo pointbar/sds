b59170f52d6588c05438ecc370a537a23db728a1
pntbr <stephane.langlois@scopyleft.fr>
2018-04-20 15:01:10 +0200

    Add geoloc

--- umap/something.js
+++ umap/something.js
@@ -4,14 +4,31 @@ const Papa = require('papaparse')
 
 fs.readFile('cities-water-prices.csv', 'utf-8', (err, result) => {
 	const cities = Papa.parse(result, {header: true}).data
-	const data = addMarkerToCities(cities)
-	const dataGeoJson = GeoJSON.parse(data , {Point: ['lat', 'lng']})
-
-	fs.writeFileSync('umap.geojson', JSON.stringify(dataGeoJson, null, 4)) 
+	addCoordsToCities(cities).then(result => {
+		result = addMarkerToCities(cities)
+		const dataGeoJson = GeoJSON.parse(result , {Point: ['lat', 'lng']})
+		fs.writeFileSync('umap.geojson', JSON.stringify(dataGeoJson, null, 4)) 
+	})
 })
 
 // FUNCTIONS
 
+function addCoordsToCities (cities) {
+	return new Promise(function(resolve, reject) {
+		fs.readFile('cities.json', 'utf-8', (err, result) => {
+			const locations = JSON.parse(result)
+			if (err) reject(err)
+			resolve(cities.map(city =>
+				locations
+					.filter((location, idx, arr) =>
+						slugify(location.name) === slugify(city.name) &&
+						slugify(locations[idx-1].name) !== slugify(city.name))
+					.map(location => Object.assign(city, {lat: location.gps_lat, lng: location.gps_lon}))
+			))
+		})
+	})
+}
+ 
 function addMarkerToCities (cities) {
 	return cities.map(city => {
 		const price = +city.description
@@ -28,4 +45,20 @@ function addMarkerToCities (cities) {
 
 		return city
 	})
+}
+
+function slugify (text) {
+	const a = 'àáäâèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;'
+	const b = 'aaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------'
+	const p = new RegExp(a.split('').join('|'), 'g')
+
+	return text.toString().toLowerCase()
+		.replace(/\s+/g, '-') // Replace spaces with -
+		.replace(p, c =>
+			b.charAt(a.indexOf(c))) // Replace special chars
+		.replace(/&/g, '-and-') // Replace & with 'and'
+		.replace(/[^\w\-]+/g, '') // Remove all non-word chars
+		.replace(/\-\-+/g, '-') // Replace multiple - with single -
+		.replace(/^-+/, '') // Trim - from start of text
+		.replace(/-+$/, '') // Trim - from end of text
 }
